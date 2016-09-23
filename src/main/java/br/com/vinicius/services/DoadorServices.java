57e072737d9fdf86edcd10f8eb263acfa730cd41
JavaRubyNode <viniciusjavaruby@gamail.com>
2016-09-23 17:30:31 -0300

    finalizando a criacao do repository Doador

--- src/main/java/br/com/vinicius/services/DoadorServices.java
+++ src/main/java/br/com/vinicius/services/DoadorServices.java
@@ -11,6 +11,9 @@ import br.com.vinicius.repository.DoadorRepository;
 public class DoadorServices  {
 
 	@Autowired
+	private Doador doadores;
+	
+	@Autowired
 	private DoadorRepository dao;
 	
 	// metodo para listar os doadores 
@@ -19,6 +22,15 @@ public class DoadorServices  {
 	// metodo para salvar o doador no banco de dados 
 	public Doador salvar(Doador doador){doador.setId(null); return dao.save(doador);}
 	
-	
+	// metodo para apagar dos registros o doador
 	public void deletar(Long id ){dao.delete(id);}
+	
+	// metodo para buscar o doador passando por parametro o id
+	public Doador buscar(Long id){doadores = dao.findOne(id); return doadores;}
+	
+	// metodo para atualizar o doador 
+	public void atualizar (Doador doador){verificarExistencia(doador);dao.save(doador);}
+	
+	// metodo para checar se o doador existe 
+	private void verificarExistencia(Doador doador){buscar(doador.getId());}
 }
