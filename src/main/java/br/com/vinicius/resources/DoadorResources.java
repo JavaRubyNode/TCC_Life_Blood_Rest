8f2546383c7b378e55424172d05cab02c1572ee9
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-09-23 23:40:36 -0300

    criado a classe que controlador Web services

--- src/main/java/br/com/vinicius/resources/DoadorResources.java
+++ src/main/java/br/com/vinicius/resources/DoadorResources.java
@@ -1,9 +1,49 @@
 package br.com.vinicius.resources;
 
+import java.net.URI;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import br.com.vinicius.domain.Doador;
+import br.com.vinicius.services.DoadorServices;
 
-@RestController("/doador")
+@RestController
+@RequestMapping("/doador")
 public class DoadorResources {
 
+	@Autowired
+	private DoadorServices doadorServices;
+	
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Doador>> listarTodos(){
+		return ResponseEntity.status(HttpStatus.OK).body(doadorServices.listar());}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> salvar(@Valid @RequestBody Doador doador){
+		doador = doadorServices.salvar(doador);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(doador.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", 
+			method = RequestMethod.DELETE,
+			headers = "Accept =application/json", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		doadorServices.deletar(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
