591e21c6d5cf775be6eecdde006f623d1ceb981d
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-09-27 15:14:12 -0300

    foi corrigido e testado todos os metodos e foi organizado o codigo

--- src/main/java/br/com/vinicius/resources/DoadorResources.java
+++ src/main/java/br/com/vinicius/resources/DoadorResources.java
@@ -2,8 +2,10 @@ package br.com.vinicius.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +26,17 @@ public class DoadorResources {
 	@Autowired
 	private DoadorServices doadorServices;
 	
+	//-------------------------------Listar todos Doadores---------------------------------------
+	
 	@CrossOrigin
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Doador>> listarTodos(){
 		return ResponseEntity.status(HttpStatus.OK).body(doadorServices.listar());}
+	
+	//--------------------------------------------------------------------------------------------
+	
+	
+	//------------------------------- Salvar Dodores ---------------------------------------------
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> salvar(@Valid @RequestBody Doador doador){
@@ -37,13 +46,49 @@ public class DoadorResources {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@RequestMapping(value = "/{id}", 
-			method = RequestMethod.DELETE,
-			headers = "Accept =application/json", 
-			produces = MediaType.APPLICATION_JSON_VALUE)
+	//-------------------------------------------------------------------------------------------
+	
+	
+	//------------------------------ Deletar Doadores -------------------------------------------
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE,
+			headers = "Accept=application/json", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
-		doadorServices.deletar(id);
+	doadorServices.deletar(id);
+	return ResponseEntity.noContent().build();
+	}
+	
+	//----------------------------------------------------------------------------------------------
+	
+	
+	//--------------------------------Buscar por Id--------------------------------------------------
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+		Doador doador = doadorServices.buscar(id);
+		
+		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
+		
+		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(doador);
+	}
+	
+	//-----------------------------------------------------------------------------------------------
+	
+	
+	//---------------------------------Atualizar dados do Doador--------------------------------------
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> atualizar(@RequestBody Doador doador,@PathVariable("id") Long id){
+		doador.setId(id);
+		doadorServices.atualizar(doador);
 		return ResponseEntity.noContent().build();
 	}
+	
+	//------------------------------------------------------------------------------------------------
+	
+	
+	
+	
 	
 }
