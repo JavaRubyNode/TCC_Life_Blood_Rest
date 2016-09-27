4a58b8464386f4d031de78dda6ca57659d72f893
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-09-27 15:04:09 -0300

    correção da interface colocando @Repository e deixando um comentario

--- src/main/java/br/com/vinicius/repository/DoadorRepository.java
+++ src/main/java/br/com/vinicius/repository/DoadorRepository.java
@@ -1,9 +1,16 @@
 package br.com.vinicius.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.vinicius.domain.Doador;
 
+@Repository
 public interface DoadorRepository extends JpaRepository<Doador, Long>{
 
+	/*
+	 * interface para realizar as transações do CRUD junto ao banco de dados os recursos
+	 * serão aplicados na classe DoadorServices 
+	 */
+	
 }
