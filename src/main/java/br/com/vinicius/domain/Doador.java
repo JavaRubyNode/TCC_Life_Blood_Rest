b826450eb5bb82f076b321ccffa16427aeafe5fe
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-11-01 12:25:17 -0200

    mudança do BeanValidation do atributo idade de NotEmpty para NotNull

--- src/main/java/br/com/vinicius/domain/Doador.java
+++ src/main/java/br/com/vinicius/domain/Doador.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -42,7 +44,7 @@ public class Doador{
 	private String redeSocial;
 	
 	@JsonInclude(Include.NON_NULL)
-	@NotEmpty(message="Idade e obrigatório")
+	@NotNull(message="Idade e obrigatório")
 	private Integer idade;
 	
 	@JsonInclude(Include.NON_NULL)
